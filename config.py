bde96b23ce91956aadedca5a6d855529d0f9078b
maramirezr04-arch <maramirezr04@liverpool.com.mx>
2026-04-18 14:48:41 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+LIVERPOOL = {
+    "url_login": "https://surtidoapp-oms.liverpool.com.mx/#/login",
+    "usuario":   "vmrangelj",
+    "password":  "Liverpool1",
+}
+
+GOOGLE = {
+    "sheet_id":       "1ln87X03CxFNTGiU125ACn7BSBoqidPZwU7GPp79IkUM",
+    "nombre_hoja":    "Hoja 1",
+    "credentials":    "/home/runner/work/liverpool-bot/liverpool-bot/credentials.json",
+    "sheet2_id":      "1baVeWCA7-fhTHVUnPgd1_67kjJQJchwwvT64K5t0OJs",
+    "sheet2_hoja":    "Hoja 1",
+    "sheet2_col":     "V",
+    "sheet2_fila":    2,
+    "timestamp_col":  "AS",
+    "timestamp_fila": 2,
+}
+
+CHAT = {
+    "webhook_url":    "https://chat.googleapis.com/v1/spaces/AAQAQ6DrmfI/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=VzOYmkn9w65FPf64JLq1ySI0VFyD5E8sdc-KQc29nXw",
+    "looker_url":     "https://lookerstudio.google.com/u/0/reporting/25b92bb8-e5e8-40a6-bc36-e39b62ff98df/page/Kn5mD",
+    "nombre_reporte": "Indicadores Liverpool 456",
+}
+
+CARPETA_DESCARGA = "/tmp/descargas"
